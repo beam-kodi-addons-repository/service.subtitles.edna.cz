8ffa01becdbd7e40a2b9c6ee084c1d5103068ba6
Beam <ondra@bimovi.cz>
2014-05-04 18:13:32 +0200

    notifikace pokud se nehledaji ani cze ani slo titulky

--- resources/lib/EdnaClient.py
+++ resources/lib/EdnaClient.py
@@ -83,6 +83,9 @@ class EdnaClient(object):
 				filter_subtitles_list.append(subtitle)
 
 		if not filter_subtitles_list:
+			if "cze" not in set_languages and "slo" not in set_languages:
+				dialog = xbmcgui.Dialog()
+				dialog.ok(self.addon.getAddonInfo('name'), self._t(32100))
 			return None
 		else:
 			filter_results_list = copy.deepcopy(subtitles_list)
